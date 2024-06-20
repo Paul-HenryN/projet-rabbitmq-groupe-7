b1503b1495f569a0927e9dda21785b3d970331b4
Paul-HenryN <ngpaulhenry@gmail.com>
2024-06-20 10:16:55 +0200

    fix(input comp): add forwardref

--- frontend/src/components/input.tsx
+++ frontend/src/components/input.tsx
@@ -1,5 +1,13 @@
+import React from "react";
+
 interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {}
 
-export function Input({ className, ...rest }: InputProps) {
-  return <input className={`border rounded-md p-2 ${className}`} {...rest} />;
-}
+export const Input = React.forwardRef<HTMLInputElement, InputProps>(
+  ({ className, ...rest }, ref) => (
+    <input
+      ref={ref}
+      className={`border rounded-md p-2 ${className}`}
+      {...rest}
+    />
+  )
+);
